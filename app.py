9b5560aeeb42f9ef7e05c10a869d7a5bc9c47cbd
vasantymarshalgmail.com <vasantymarshal@gamil.com>
2024-06-28 14:30:38 +0200

    add - @route

--- app.py
+++ app.py
@@ -1,9 +1,46 @@
-from flask import Flask
+from flask import request, render_template, Flask, redirect, url_for
+import json
+
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return "Hello world"
 
-if __name__ == "__main__":
-    app.run()
+def load_blog_posts():
+    with open('blog_posts.json', 'r') as file:
+        return json.load(file)
+
+
+def save_blog_posts(posts):
+    with open('blog_posts.json', 'w') as file:
+        json.dump(posts, file, indent=4)
+
+
+@app.route('/')
+def index():
+    blog_posts = load_blog_posts()
+    return render_template('index.html', posts=blog_posts)
+
+
+@app.route('/add', methods=['GET', 'POST'])
+def add():
+    if request.method == 'POST':
+        title = request.form['title']
+        author = request.form['author']
+        content = request.form['content']
+
+        new_post = {
+            "title": title,
+            "author": author,
+            "content": content
+        }
+
+        blog_posts = load_blog_posts()
+        blog_posts.append(new_post)
+        save_blog_posts(blog_posts)
+
+        return redirect(url_for('index'))
+
+    return render_template('add.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
